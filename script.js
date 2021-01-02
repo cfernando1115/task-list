2cd5cee4bb0e3af940fb36f544b285a4342664fc
Carmen <cfernando1115@gmail.com>
2021-01-02 12:04:34 -0500

    Set up session storage and initial data loading

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-fetch('http://discoveryvip.com/shared/json.php?f=list')
+const taskTable=document.getElementsByTagName("tbody")[0];
+
+
+var getTasks=function(){
+    return fetch('http://discoveryvip.com/shared/json.php?f=list')
     .then(response=>{
         console.log(response);
         if(response.status!=200){
@@ -8,10 +12,64 @@ fetch('http://discoveryvip.com/shared/json.php?f=list')
     })
     .then(data=>{
         let tasks=data;
-        console.log(tasks);
-        console.log(tasks[0].info);
+        tasks.forEach((el)=>{
+            tasks[el.key]=el.value;
+        })
+        return tasks;
     })
     .catch(err=>console.log(err));
+}
+
+window.onload=function(){
+    if(sessionStorage['data']){
+        let tasks=JSON.parse(sessionStorage['data']);
+        //render tasks
+        renderTasks(tasks);
+    }
+    else{
+        getTasks()
+            .then(tasks=>{
+                renderTasks(tasks);
+                let data=JSON.stringify(tasks);
+                //move to save function...
+                saveSession(data);
+            });
+    }
+    var completeBox=document.querySelectorAll("input[name=complete]");
+    completeBox.forEach(el=>{
+        el.addEventListener('change',function(){
+            el.hasAttribute('checked')?el.removeAttribute('checked'):el.setAttribute('checked','checked');
+            //(el.getAttribute('value'))?el.setAttribute('value','false'):el.setAttribute('value','true');
+            let status=el.getAttribute('value');
+            if(status=='true'){
+                el.setAttribute('value','false');
+            }
+            else{
+                el.setAttribute('value','true');
+            }
+            console.log(el.getAttribute('value'));
+        })
+    })
+}
+
+const renderTasks=function(tasks){
+    let html='';
+    tasks.forEach((el)=>{
+        html+=`
+            <tr>
+                <td><input name="complete" type="checkbox" value="${el.checked?'true':'false'}" ${el.status?'checked':''}></td>
+                <td>${el.info}</td>
+                <td><input name="delete" type="checkbox"></td>
+            </tr>
+        `
+    });
+    taskTable.insertAdjacentHTML('beforeend',html);
+}
+
+const saveSession=function(data){
+    sessionStorage.setItem('data', data);
+}
+
 
 
         /*var myForm=document.getElementById('myForm');
